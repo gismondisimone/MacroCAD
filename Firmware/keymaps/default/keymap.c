8b1a62b946296a9f00ccc47e8ec43f10e3a75bc7
gismondisimone <gismondi.simone09@gmail.com>
2026-08-15 23:27:15 +0200

    Inizializzazione completa MacroCAD

---
+++ Firmware/keymaps/default/keymap.c
@@ -0,0 +1,30 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ 7 │ 8 │ 9 │
+     * ├───┼───┼───┤
+     * │ 4 │ 5 │ 6 │
+     * ├───┼───┼───┤
+     * │ 1 │ 2 │ 3 │
+     * ├───┼───┼───┤
+     * │ 0 │ . │Ent│
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3,
+        KC_P0,   KC_PDOT, KC_PENT
+    )
+};
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    oled_write_ln("  MacroCAD  ", false);
+    return false;
+}
+#endif
---
+++ Firmware/readme.md
@@ -0,0 +1,13 @@
+# macrocad
+
+MacroCAD is a KeyPad Design by a CAD user for CAD users
+
+* Keyboard Maintainer: [Simone](https://github.com/gismondisimone)
+* Hardware:** Seeed XIAO RP2040
+* Hardware Availability: https://github.com/gismondisimone/MacroCAD
+
+
+
+## Bootloader
+
+Per entrare in modalità bootloader, tieni premuto il tasto BOOT, premi e rilascia RESET, quindi rilascia BOOT.
